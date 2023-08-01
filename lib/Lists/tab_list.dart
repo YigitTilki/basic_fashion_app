99aef7d398f9868b8a09f7144ff12ff80d4b117e
Yiğit TİLKİ <bestnemesis1234@gmail.com>
2023-08-01 13:07:24 +0300

    lists created

---
+++ lib/Lists/tab_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+List<Tab> tabList = [
+  const Tab(
+    icon: Icon(Icons.more, color: Colors.grey, size: 22),
+  ),
+  const Tab(
+    icon: Icon(Icons.play_arrow, color: Colors.grey, size: 22),
+  ),
+  const Tab(
+    icon: Icon(Icons.navigation, color: Colors.grey, size: 22),
+  ),
+  const Tab(
+    icon: Icon(Icons.supervised_user_circle, color: Colors.grey, size: 22),
+  ),
+];
